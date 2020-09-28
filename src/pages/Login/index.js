aaf84fde710023f7a76311b13e85f0c47b429437
Lunathos <ivadcgs1999@gmail.com>
2020-09-27 22:32:39 -0300

    First commit

---
+++ src/pages/Login/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function Login() {
+
+    return (
+        
+        <div className="login-container">
+            <div className="form-container">
+                <section className="form">
+                <img src={logoImg} alt="Google" />
+                    <form>
+                        <h1>Fazer Login</h1>
+                        <h3>Ir para o Gmail</h3>
+
+                        <input placeholder="E-mail ou telefone"/>
+                        <h4><a href="/reset">Esqueceu seu e-mail?</a></h4>
+
+                        <p>Não está no seu computador? Use o modo visitante para
+                        <br/> fazer login com privacidade. <a href="/learnmore">Saiba mais</a></p>                    
+                        
+                        <a href="/signup">Criar Conta</a>
+                        <button className="button" type="submit">Próxima</button>
+
+                        
+                    </form>
+                </section>
+            </div>
+        </div>
+    );
+}
