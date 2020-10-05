73951984910bf9cd9de42dbe6c92682f70f8a33c
Lunathos <ivadcgs1999@gmail.com>
2020-10-05 12:55:44 -0300

    Add: Input, Icons; Adjust: Css styles

--- src/pages/Gmail/index.js
+++ src/pages/Gmail/index.js
@@ -3,8 +3,13 @@ import React, {useState} from 'react';
 import {
     FaInbox, FaStar, FaClock,
     FaPaperPlane, FaStickyNote, FaExclamationCircle,
-    FaTrash, FaSearch, FaQuestionCircle, FaCog, FaTh
+    FaTrash, FaSearch, FaQuestionCircle, FaCog, FaTh,
+    FaRedo, FaCaretDown, FaEllipsisV, FaLink,
+    FaSmile , FaGoogleDrive, FaImage, FaLock,
+    FaPenAlt, FaRegUserCircle
 } from 'react-icons/fa';
+
+import { FiSquare } from 'react-icons/fi';
 
 import SideNav, {
     NavItem, NavIcon, NavText
@@ -25,14 +30,31 @@ import './styles.css';
 //         </div>
 //     )
 // }
+//                                    <button 
+// type="button"
+// className="bts-clip"
+// onClick={() => {
+//     setAnex(!anex);
+// }}
+// >
+
+// <FaPaperclip size={17.5}/>
+// </button>
 
 export default function Gmail(){
 
     const [disp, setDisp] = useState(false);
+    const [anex, setAnex] = useState(false);
 
-    // function handleAlert() {
-    //     alert('Escreva seu e-mail!');
-    // }
+    function onChange() {
+        setAnex(!anex);
+    }
+
+    function handleSumbit(event) {
+        alert('E-mail enviado');
+        setDisp(!disp);
+        event.preventDefault();
+    }
 
     return(
         <div className="gmail-container">
@@ -46,10 +68,14 @@ export default function Gmail(){
                     </div>
                     <form>
                         <input
-                            placeholder="Pesquisar e-mail">
+                            placeholder="Pesquisar e-mail"
+                            type="text"
+                            >
                         </input>
                     </form>
                 </div>
+
+                <div className="g-s-icon"/>
 
                 <div className="icon-group">
                     <button className="q-bts-icon">
@@ -63,10 +89,15 @@ export default function Gmail(){
                     <button className="t-bts-icon">
                         <FaTh size={16}/>
                     </button>
+
+                    <button className="u-bts-icon">
+                        <FaRegUserCircle size={35}/>
+                    </button>
                 </div>
 
-                <div className="google-left-navbar">
+                
 
+                <div className="google-left-navbar">
                     <SideNav
                         onSelect={(selected) => {
                             // Add your code here
@@ -148,14 +179,25 @@ export default function Gmail(){
                     </SideNav>
                 </div>
 
-
             </div>
 
             <div className="gmail-body">
 
                 <div className="gmail-body-header">
-                    <p>Oi</p>
-                    <p>Hwat</p>
+                    <button className="bts-select">
+                        <FiSquare size={17.5}/>
+                        <FaCaretDown size={17.5}/>
+                    </button>
+                    
+                    <div className="gmail-b-header"/>
+                    <button className="bts-redo">
+                            <FaRedo size={17.5}/>
+                        </button>
+
+                        <button className="bts-grid">
+                            <FaEllipsisV size={17.5}/>
+                        </button>
+
                 </div>
                 
                 <div className="gmail-body-container">
@@ -166,7 +208,7 @@ export default function Gmail(){
                                     <FaStar size={17.5}/>
                                 </button>
 
-                                <button class="bts-mail">
+                                <button className="bts-mail">
                                     <strong className="title-mail">TeT cursos</strong>
                             
                                     <strong className="desc-mail">Gmail simulador</strong>
@@ -179,22 +221,92 @@ export default function Gmail(){
 
             
                 </div>
-
-                <form>
+                    
                         {disp &&
                         <div className="escrever-mail-box" style={{ display: disp}}>
                             <div className="escrever-header">
                                 <h2>Nova mensagem</h2>
                             </div>
-                            <form>
-                                <input placeholder="Destinatários" type="text"/>
+                            <form onSubmit={handleSumbit}>
+                                <input placeholder="Para" type="text"/>
                                 <input placeholder="Assunto" type="text"/> 
-                                <textarea></textarea>      
-                            </form>
-                        </div>}
-                    </form>    
+                                <textarea></textarea>   
+                                                                
+                                {anex &&    <>
+                                <div style={{display: anex}}>
+                                    
+                                    <div className="fileUpload">
+                                        <input type="file"  />
+                                    </div>
+                                    </div>
+                                    </>
+                                    
+                                }
+                                
+                                
+                            
 
+                            <div className="escrever-footer">
+                                <div className="send-group">
+                                    <button 
+                                        className="button"
+                                        type="submit"
+                                    > Enviar
+                                    </button>
+                                    <div className="bts-caret">
+                                        <FaCaretDown size={17.5}/>
+                                    </div>
+                                </div>
+                            
+                                <div className="bts-group-footer">
+
+                                    <button
+                                        type="button"
+                                        onClick={onChange}
+                                        className="bts-link">
+                                        <FaLink className="bts-link" size={17.5} />
+                                    </button>
+
+                                    <button 
+                                    type="button"
+                                    className="bts-smile"
+                                    >
+                                        <FaSmile size={17.5} />
+                                    </button>
+
+                                    <button type="button" className="bts-drive">
+                                        <FaGoogleDrive size={17.5} />
+                                    </button>
+
+                                    <button type="button" className="bts-image">
+                                        <FaImage size={17.5} />
+                                    </button>
+                                            
+                                    <button type="button" className="bts-lock">
+                                        <FaLock size={17.5} />
+                                    </button>
+
+                                    <button type="button" className="bts-pen">
+                                        <FaPenAlt size={17.5} />
+                                    </button>
+                                            
+                                    <button type="button" className="bts-eli">
+                                        <FaEllipsisV size={17.5} />
+                                    </button>
+                                            
+                                    <button type="button" className="bts-trash">
+                                        <FaTrash size={17.5} />
+                                    </button>                     
+                                </div>
+                            </div>
+                            </form></div>}         
+ 
             </div>
+
+
+            <div className="gmail-footer">
+            </div>         
+
         </div>
     );
 }
